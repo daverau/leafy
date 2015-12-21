b89b61dea982d82f10f64ad2bfba2d7859083247
Dave Rau <dave@redlabor.com>
2015-12-21 04:43:18 -0800

    renaming stuff, adding readme

--- preload.js
+++ preload.js
@@ -1,18 +1,18 @@
 // # Preload
 function preload() {
 
-  game.load.spritesheet('leafy', 'img/leafy.png', 128, 128);
-  game.load.image('ground', 'img/ground.png');
+  game.load.spritesheet('leafy', 'assets/leafy.png', 128, 128);
+  game.load.image('ground', 'assets/ground.png');
   
   // just using the color for now...
-  game.load.image('tree', 'img/tree.png');
+  game.load.image('tree', 'assets/tree.png');
 
   // will replace with code-generated trees at some point...
-  game.load.image('tree1', 'img/tree1.png');
-  game.load.image('tree2', 'img/tree2.png');
-  game.load.image('tree3', 'img/tree3.png');
-  game.load.image('tree4', 'img/tree4.png');
-  game.load.image('tree5', 'img/tree5.png');
-  game.load.image('tree6', 'img/tree6.png');
+  game.load.image('tree1', 'assets/tree1.png');
+  game.load.image('tree2', 'assets/tree2.png');
+  game.load.image('tree3', 'assets/tree3.png');
+  game.load.image('tree4', 'assets/tree4.png');
+  game.load.image('tree5', 'assets/tree5.png');
+  game.load.image('tree6', 'assets/tree6.png');
 
 }
