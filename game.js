0160950d1593078091e5b6853797621b29edd51c
Dave Rau <dave@redlabor.com>
2015-12-21 04:59:40 -0800

    moving files around

--- game.js
+++
@@ -1,9 +0,0 @@
-// # Leafy
-// a phaser game experiment
-// by dave rau
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'leafy', { preload: preload, create: create, update: update, render: render } );
-
-
-
-
