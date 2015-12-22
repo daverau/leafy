9a24392401278397d95ab4dd50bac28a2051e718
Dave Rau <dave@redlabor.com>
2015-12-21 18:49:50 -0800

    moved trees up in the world gen so they're in the bg, still don't understand moveUp() etc and sprite.z

--- js/create.js
+++ js/create.js
@@ -1,58 +1,50 @@
 // # Create
 function create() {
 
+  // # World gen
   game.stage.backgroundColor = 0xF4F4F4;
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-//game.physics.arcade.gravity.y = 300;
+  //game.physics.arcade.gravity.y = 300;
+  game.world.setBounds(0, 0, vars.worldSize, game.height);
+
+  // # Trees
+  game.trees = game.add.group();
+  game.trees.enableBody = true;
+  // Test generate some random trees
+  for (x = 0; x < (vars.worldSize * .01); x++) {
+    genTree();
+  }
 
   // # Player (Leafy) setup
   game.leafy = game.add.sprite( vars.worldSize / 2 , 10, 'leafy');
-  //walk = game.leafy.animations.add('walk');
-  //game.leafy.animations.play('walk', 15, true);
-  game.leafy.scale.setTo(.5);
-  game.leafy.anchor.setTo(.5, 1); //so it flips around its middle
+  game.physics.arcade.enable(game.leafy);
+  game.leafy.body.gravity.y = 1000;
+  game.leafy.body.maxVelocity.y = 500;
+  //game.leafy.scale.setTo(.5);
 
-  //game.leafy.animations.add('left', [0, 1, 2, 3, 4, 5, 6], 10, true);
+  //animations for walkcycle
   game.leafy.animations.add('turn', [7], 0, true);
   game.leafy.animations.add('walk', [0, 1, 2, 3, 4, 5, 6], 10, true);
   game.leafy.animations.add('jump', [1], 0, true);
+  game.leafy.anchor.setTo(.5, 1); //flip at middle point
 
+  // [todo adjust box hit size
+  //game.leafy.body.setSize(20, 32, 5, -6);
+  
   //ground
   game.ground = game.add.tileSprite(0 , game.height-60, vars.worldSize, 60, 'tree');
-
-	//enable physics on the player and ground
-  game.physics.arcade.enable(game.leafy);
   game.physics.arcade.enable(game.ground);
-
-  //player gravity
-	game.leafy.body.gravity.y = 1000;
-  game.leafy.body.maxVelocity.y = 500;
-  game.camera.follow(game.leafy);
- 
-  //ground
 	game.ground.body.immovable = true;
 	game.ground.body.allowGravity = false;
  
-  //[todo] needs tweaking
-  game.leafy.body.setSize(20, 32, 5, -6);
-  //game.leafy.body.collideWorldBounds=true;
-  
+  // # Camera follows player
+  game.camera.follow(game.leafy);
+
   // [todo] out of bounds kill should respawn
   game.leafy.outOfBoundsKill = true;
+  //game.leafy.body.collideWorldBounds=true;
 
 
-  // # World gen
-
-	// ## Trees
-	game.trees = game.add.group();
-	game.trees.enableBody = true;
-	// Test generate some random trees
-	for (x = 0; x < (vars.worldSize * .01); x++) {
-		genTree();
-	}
-
-  // [todo] update bounds with global variable
-  game.world.setBounds(0, 0, vars.worldSize, game.height);
 
   // input
   cursors = game.input.keyboard.createCursorKeys();
