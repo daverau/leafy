1cc0fbefe285266c033479c18b06ef9b8ed49916
Dave Rau <dave@redlabor.com>
2015-12-23 06:13:41 -0800

    code cleanup and wordsmithing

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,7 @@ function genTree() {
 
 	var tree = {};
 
-	// # Generate
+	// ## Generate
   // height
 	var heightMin = 32;
 	var heightMax = 300;
@@ -28,46 +28,61 @@ function genTree() {
 
   // leaf color
   var colorLeaf = ['green', 'yellow', 'red'];
-  tree.colorLeaf = colorLeaf[Math.floor(Math.random()*colorLeaf.length)];;
+  tree.colorLeaf = colorLeaf[Math.floor(Math.random()*colorLeaf.length)];
 
   // branch color
   var colorBranch = ['brown','grey'];
-  tree.colorBranch = colorBranch[Math.floor(Math.random()*colorBranch.length)];;
+  tree.colorBranch = colorBranch[Math.floor(Math.random()*colorBranch.length)];
 
   // foreground vs background
   var ground = ['bg', 'foreground']
-  tree.ground = ground[Math.floor(Math.random()*ground.length)];;
+  tree.ground = ground[Math.floor(Math.random()*ground.length)];
 
-  // # Draw
+  // ## Draw
   // draw a tree
   // draw trunk
   // draw second color
   // attach branches
   // attach leaves
 
-  // # We've got a tree
-  // console.log( tree );
-  // return tree;
-
-
-  // # Draw trees
-  // get tree count based on world size
-  var x = game.rnd.integerInRange(0, vars.worldSize);
-  //
-  var treeimg = 'tree' + Math.floor(Math.random()*5+1);
-  // 
-  if ( Math.abs(x) < (vars.worldSize*.3) ) {
-    treeimg = 'tree' + Math.floor(Math.random()*10+1);
-  }
-  var t = game.trees.create(x, 0, treeimg);
-  t.y = game.world.height - (t.height + 58);
-  //t.z = -100;
-
+  // ## dynamic drawn "trees" as rectangles
   //var t = game.trees.create(x, game.height-(tree.height * 2), 'tree');
   //t.scale.setTo(.5);
   //t.alpha = .5;
   //t.height = tree.height * 2;
   //t.width = tree.width * 2;
+
+  // ## We've got a tree
+  // console.log( tree );
+  // return tree;
+
+  // get random x value for tree
+  var x = game.rnd.integerInRange(0, vars.worldSize);
+
+  // full random spectrum
+  // var treeimg = 'tree' + Math.floor(Math.random()*10+1);
+  
+  // ## group trees by world depth
+  if ( Math.abs(x) < (vars.worldSize*.8) ) {
+    treeimg = 'tree' + Math.floor(Math.random()*4+1);
+  }
+  if ( Math.abs(x) < (vars.worldSize*.3) ) {
+    treeimg = 'tree' + Math.floor(Math.random()*10+1);
+  }
+
+  // draw
+  var t = game.trees.create(x, 0, treeimg);
+  t.y = game.world.height - (t.height + 58); // magic number based on ~ground.height
+
+  // ## front trees
+  // [todo/question] why no workie?
+  // t.z = 1000;
+  // game.world.bringToTop(t);
+  // var randPool = ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','2'];
+  // var rand = randPool[Math.floor(Math.random()*randPool.length)];
+  // if (rand == 2) {
+  //   game.world.bringToTop(t);
+  // }
 
 }
 
