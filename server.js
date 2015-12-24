cdb2630dd6efe1d199a40772726d8944b7c32a6e
Dave Rau <dave@redlabor.com>
2015-12-24 11:05:50 -0800

    removed ignore rules and live reload works

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var liveServer = require("live-server");
-
 var params = {
-    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
-    wait: 300 // Waits for all changes, before reloading. Defaults to 0 sec.
+    file: "index.html",
+    wait: 0
 };
-
 liveServer.start(params);
