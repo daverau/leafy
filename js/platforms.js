860b2f564a0700a15ec27df512640d29e7aaa3b2
Dave Rau <dave@redlabor.com>
2016-07-18 19:38:29 -0400

    optimized platform code by removing vars for reduced garbage collection, see how this runs on the phone and then call it a version bump.

--- js/platforms.js
+++ js/platforms.js
@@ -13,6 +13,60 @@ function placePlatforms() {
       // far platform.x
       var maxPlatformX = getLastPlatformX();
 
+      var heights = vars.platformLevels[isLevel(maxPlatformX)].heights;
+      var h = vars.platformHeights[vars.platformLevels[isLevel(maxPlatformX)].heights[Math.floor(Math.random() * vars.platformLevels[isLevel(maxPlatformX)].heights.length)] -1];
+
+      // set a platform ID
+      if (!platform.i) {
+        platform.i = i;
+      }
+
+      // apply platform
+      // an optimized, but unreadable mess
+      // JS garbage collection is interesting
+      platform.score = vars.platformGaps[vars.platformLevels[isLevel(maxPlatformX)].gaps[Math.floor(Math.random()*vars.platformLevels[isLevel(maxPlatformX)].gaps.length)] -1];
+      platform.touched = false;
+      platform.reset(maxPlatformX + vars.platformGaps[vars.platformLevels[isLevel(maxPlatformX)].gaps[Math.floor(Math.random()*vars.platformLevels[isLevel(maxPlatformX)].gaps.length)] -1], game.height - h);
+      platform.width = vars.platformWidths[vars.platformLevels[isLevel(maxPlatformX)].widths[Math.floor(Math.random()*vars.platformLevels[isLevel(maxPlatformX)].widths.length)] -1];
+      platform.nextX = maxPlatformX + vars.platformGaps[vars.platformLevels[isLevel(maxPlatformX)].gaps[Math.floor(Math.random()*vars.platformLevels[isLevel(maxPlatformX)].gaps.length)] -1];
+      platform.height = vars.platformHeight;
+
+      // coins?
+      var items = [1,1,2,3];
+      var yesCoins = items[Math.floor(Math.random()*items.length)];
+      // 1 === do nothing
+      // coins
+      if (yesCoins === 2) {
+        if (platform.width > 50) {
+          placeCoins(platform);
+        }
+      // coin rings
+      } else if (yesCoins === 3) {
+        placeBluering(platform);
+      }
+
+      // [todo] place trees for this platform
+
+    } else {
+      //console.log('...no dead platforms...');
+    }
+  }
+}
+
+
+function placePlatformsUnoptimized() {
+  var platformCount = game.platforms.children.length;
+  //console.log('placePlatforms(): '+platformCount);
+
+  for (var i=0; i<platformCount; i++) {
+
+    // get from pool
+    var platform = game.platforms.getFirstDead();
+    if (platform) {
+
+      // far platform.x
+      var maxPlatformX = getLastPlatformX();
+
       // platform settings per level
       var widths = vars.platformLevels[isLevel(maxPlatformX)].widths;
       var w = vars.platformWidths[widths[Math.floor(Math.random()*widths.length)] -1];
