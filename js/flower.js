c2f12904b1e2ab7d69fa84841d88955f2f738cf7
Dave Rau <dave@redlabor.com>
2016-08-14 11:15:50 -0700

    rotating acorns for more juice

--- js/flower.js
+++ js/flower.js
@@ -3,19 +3,20 @@
 function genCoins() {
   game.coins = game.add.group();
   game.coins.enableBody = true;
-  for (var i=0; i<vars.coinTotal; i++) {
-    var coin = new Blueleaf(game, -100, game.height-vars.platformHeight);
+  for (var i = 0; i < vars.coinTotal; i++) {
+    var coin = new Blueleaf(game, -100, game.height - vars.platformHeight);
     game.coins.add(coin);
   }
 }
 
-Blueleaf = function (game, x, y) {
+Blueleaf = function(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, "blueleaf");
-  this.anchor.setTo(0.5,1);
+  this.anchor.setTo(0.5, 1);
   this.pickedup = false;
   this.enableBody = true;
   game.physics.arcade.enable(this);
   this.body.velocity.x = vars.gameSpeed;
+  this.angle = 5;
 
   // animations
   this.tween = game.add.tween(this).to({
@@ -23,18 +24,31 @@ Blueleaf = function (game, x, y) {
     x: (game.width - 100),
     y: (100),
   }, 1000, Phaser.Easing.Cubic.Out);
+  tweenRotation(this);
 };
+
+// nice resuable tween function
+// from http://www.html5gamedevs.com/topic/17632-rotate-back-and-forth-using-tween/
+function tweenRotation(object) {
+  var randomDelay = game.rnd.between(0, 900);
+  var amountRotation = 5;
+  object.angle = amountRotation * -1;
+  object.anchor.set(.5);
+  var move = game.add.tween(object).to({
+    angle: amountRotation,
+  }, 800, Phaser.Easing.Quadratic.InOut, true, randomDelay, -1, true);
+}
 
 Blueleaf.prototype = Object.create(Phaser.Sprite.prototype);
 Blueleaf.prototype.constructor = Blueleaf;
 // Blueleaf.prototype.update = function() {
-  //resetLeaf(this);
-  // moved to platforms.js blue ring stuff
+//resetLeaf(this);
+// moved to platforms.js blue ring stuff
 
-  // [todo] magnet so coins move toward leafy when he's close
-  // if ( game.physics.arcade.distanceBetween(game.leafy, this) < 100 ) {
-  //   game.physics.arcade.moveToObject( this, game.leafy, 200);
-  // }
+// [todo] magnet so coins move toward leafy when he's close
+// if ( game.physics.arcade.distanceBetween(game.leafy, this) < 100 ) {
+//   game.physics.arcade.moveToObject( this, game.leafy, 200);
+// }
 
 // };
 
@@ -42,7 +56,7 @@ function genBlueRings() {
   game.bluerings = game.add.group();
   game.bluerings.enableBody = true;
 
-  for (var i=0; i<vars.blueRingTotal; i++) {
+  for (var i = 0; i < vars.blueRingTotal; i++) {
     var bluering = game.add.group();
     bluering.enableBody = true;
 
@@ -56,10 +70,10 @@ function genBlueRings() {
     leaf = new Blueleaf(game, -70, 20);
     bluering.add(leaf);
 
-    leaf = new Blueleaf(game, 66*2, 30*2);
+    leaf = new Blueleaf(game, 66 * 2, 30 * 2);
     bluering.add(leaf);
 
-    leaf = new Blueleaf(game, -66*2, 30*2);
+    leaf = new Blueleaf(game, -66 * 2, 30 * 2);
     bluering.add(leaf);
 
     bluering.y = game.height - 280;
@@ -97,17 +111,17 @@ function genFlowers() {
   game.flowers = game.add.group();
   game.flowers.enableBody = true;
   //console.log('flowers: '+fcount);
-  for (x=0; x<vars.flowerCount; x++) {
+  for (x = 0; x < vars.flowerCount; x++) {
     var worldx = Math.floor(Math.random() * (game.width * 4) + (game.width * 2.5));
-    var flower = new Flower(game, worldx, game.height-vars.platformHeight);
+    var flower = new Flower(game, worldx, game.height - vars.platformHeight);
     game.flowers.add(flower);
     //console.log('flower created at x:'+flower.x);
   }
 }
 
-Flower = function (game, x, y) {
+Flower = function(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, "flower");
-  this.anchor.setTo(0.5,1);
+  this.anchor.setTo(0.5, 1);
   this.scale.setTo(0.5);
   game.physics.arcade.enable(this);
   this.body.velocity.x = vars.gameSpeed;
@@ -124,7 +138,7 @@ Flower = function (game, x, y) {
 Flower.prototype = Object.create(Phaser.Sprite.prototype);
 Flower.prototype.constructor = Flower;
 Flower.prototype.update = function() {
-  if ( offCamera(this) ) {
+  if (offCamera(this)) {
     //console.log('move a flower...');
     resetMove(
       this,
